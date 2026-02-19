be81c0309fcbca6b61e2796187ccb27b8882ebe3
kindxxxx <sava.molchanov.07@gmail.com>
2026-02-19 15:04:33 +0500

    Add simple class definition with prints

---
+++ classes/class_definition.py
@@ -0,0 +1,18 @@
+class Person:
+    name = "Saveliy"
+    age = 18
+
+p = Person()
+
+print(p.name)
+print(p.age)
+
+
+class Car:
+    brand = "Toyota"
+    year = 2020
+
+car = Car()
+
+print(car.brand)
+print(car.year)
