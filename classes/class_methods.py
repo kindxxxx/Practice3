56334de9e158539fc5a2d81fe7d5af014e1ddb94
kindxxxx <sava.molchanov.07@gmail.com>
2026-02-19 15:11:05 +0500

    Add class methods with printed results

---
+++ classes/class_methods.py
@@ -0,0 +1,15 @@
+class Calculator:
+    def add(self, a, b):
+        print(a + b)
+
+calc = Calculator()
+calc.add(3, 5)
+
+
+
+class BankAccount:
+    def deposit(self, amount):
+        print("Deposited:", amount)
+
+account = BankAccount()
+account.deposit(1000)
